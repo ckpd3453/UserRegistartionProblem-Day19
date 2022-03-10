cfb3f369770d7298fff7c947acdf08f8e6208c7d
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-10 20:22:53 +0530

    Added UC-3 Problem

--- src/com/UserRegistration/EmailValidation.java
+++ src/com/UserRegistration/EmailValidation.java
@@ -10,7 +10,7 @@ public class EmailValidation {
 		 * 1.Taking user input of first name and last name
 		 */
 		Scanner sc = new Scanner(System.in);
-		System.out.println("(hint: xyz45@gmail.com)");
+		System.out.println("(hint: abc45@bl.co.in)");
 		System.out.print("Enter the Email Id: ");
 		String email = sc.nextLine();
 		System.out.println(id(email));
@@ -21,6 +21,6 @@ public class EmailValidation {
 	 * 2.created method to check 1st letter is capital and has minimum 3 letters
 	 */
 	public static boolean id(String nameValidation) {
-		return nameValidation.matches("[a-z]{3,}[0-9]{2,}@{1}[a-z]{2,}[.][a-z]{3}");
+		return nameValidation.matches("([a-z]+[0-9]+@bl[.]co[.]in)*");
 	}
 }
